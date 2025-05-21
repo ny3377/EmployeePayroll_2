027260c78c8a1d1b9981abcde783b55ff11c3998
ny3377 <yeshonichenametla@gmail.com>
2025-05-21 12:41:51 +0530

    UC1:Added Employee Data

---
+++ src/main/java/com/example/PayrollApp/PayrollAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.PayrollApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrollAppApplication.class, args);
+	}
+
+}
